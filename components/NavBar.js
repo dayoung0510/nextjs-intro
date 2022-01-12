ffa276b3a6cab8113fad20530e0a8c3911794c2d
Dayoung <dy.kim@datadriven.kr>
2022-01-13 00:44:31 +0900

    style jsx 활용한 스타일링 실습

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,31 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${router.pathname === "/" && styles.active} ${
-            styles.link
-          }`}
-        >
-          Home
-        </a>
+        <a className={`${router.pathname === "/" && "active"}`}>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={`${router.pathname === "/about" && styles.active} ${
-            styles.link
-          }`}
-        >
-          About
-        </a>
+        <a className={`${router.pathname === "/about" && "active"}`}>About</a>
       </Link>
+
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+          padding: 1rem;
+        }
+        a {
+          color: white;
+          text-decoration: none;
+          margin-right: 1rem;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
